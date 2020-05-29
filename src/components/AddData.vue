e95a1b58c553777eb94966276ef164701d83d606
kdh976 <dae9341@gmail.com>
2020-05-29 15:22:32 +0900

    vue 수정

--- src/components/AddData.vue
+++ src/components/AddData.vue
@@ -13,18 +13,21 @@
                 </div>
 
                 <div class="isExpend">
-                    <button>수입</button>
-                    <button>지출</button>
+                    <button v-on:click="isExpendClickFunc()" v-bind:class="{on: !isExpend}">수입</button>
+                    <button v-on:click="isExpendClickFunc()" v-bind:class="{on: isExpend}">지출</button>
                 </div>
 
-                <div class="category">
-                    <a href="" v-for="item in category_in">{{item}}</a>
+                <div class="category" v-if="!isExpend">
+                    <a href="#" v-for="item in category_in">{{item}}</a>
+                </div>
+                <div class="category" v-else="">
+                    <a href="#" v-for="item in category_ex">{{item}}</a>
                 </div>
 
                 <div class="spendType">
-                    <button>신용카드</button>
-                    <button>체크카드</button>
-                    <button>현금</button>
+                    <button v-on:click="spendTypeClickFunc('sin')" v-bind:class="{on: spendType === 'sin'}">신용카드</button>
+                    <button v-on:click="spendTypeClickFunc('chk')" v-bind:class="{on: spendType === 'chk'}">체크카드</button>
+                    <button v-on:click="spendTypeClickFunc('hy')" v-bind:class="{on: spendType === 'hy'}">현금</button>
                 </div>
             </div>
         </div>
@@ -37,7 +40,9 @@
         data(){
             return {
                 isOpen:true,
-                isExpend:"none",
+                isExpend:false,
+                spendType:"sin", //sin:신용카드, chk:체크카드, hy:현금
+                category:"none",
                 category_in:["급여","용돈","이월","기타","자산인출"],
                 category_ex:["식비","교통비","문화/생활","생필품","의류","미용","의료/건강","교육","통신비","회비","경조사","저축"]
             }
@@ -55,6 +60,24 @@
                 today = yyyy.toString() + "-" + mm.toString() + "-" + dd.toString();
 
                 return today;
+            },
+
+            isExpendClickFunc: function () {
+                this.isExpend = !this.isExpend;
+            },
+
+            spendTypeClickFunc: function (type) {
+                this.spendType = type;
+            },
+
+            setMoneyBookDate: function () {
+                var setData ={
+                    date: "",
+                    isExpend:"",
+                    category: "",
+                    spendValue: 0,
+                    spendType : ""
+                }
             }
         }
 
